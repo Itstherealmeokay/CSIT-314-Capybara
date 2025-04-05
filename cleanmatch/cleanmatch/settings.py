516547d88c708ddebc7693c02aa8287e1ba82193
Chingz Com <gohchingye@gmail.com>
2025-04-05 20:24:38 +0800

    setting.py

--- cleanmatch/cleanmatch/settings.py
+++ cleanmatch/cleanmatch/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'cleanmatch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cleanmatch_db',
+        'USER': 'Cf',
+        'PASSWORD': '140401',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
