756139ce11ecbbbb77e1f454dcfad8aee967570e
Juxarius <exdarkrb@gmail.com>
2025-04-21 16:31:58 +0800

    Adding to admin.py do that these object trackers show up in the admin UI

--- capybara_clean/webapp/admin.py
+++ capybara_clean/webapp/admin.py
@@ -3,3 +3,6 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 admin.site.register(CustomUser, UserAdmin)
+
+from .models import Homeowner, Cleaner, Property, CleaningRequest
+admin.site.register([Homeowner, Cleaner, Property, CleaningRequest])
